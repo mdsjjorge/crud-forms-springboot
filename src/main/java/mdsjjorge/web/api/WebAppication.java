5d417b87c7b1a3a42b716d58b632bcae54105c1a
mdsjjorge <mdsousa.jorge@gmail.com>
2023-03-18 09:34:32 -0300

    commit inicial

---
+++ src/main/java/mdsjjorge/web/api/WebAppication.java
@@ -0,0 +1,13 @@
+package mdsjjorge.web.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppication.class, args);
+	}
+
+}
---
+++ src/main/java/mdsjjorge/web/api/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package mdsjjorge.web.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/")
+	public String Welcome() {
+		return "hello world!";
+	}
+}
